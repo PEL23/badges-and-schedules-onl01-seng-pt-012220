ba97fe8c2a14f212b533bbe0fae40ee88854a6c3
excellent-validator-4322 <plarmony24@gmail.com>
2020-02-02 17:27:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+  
+end
+
+def batch_badge_creator(names)
+  new_array = []
+  names.each do |name|
+    new_array.push {badge_maker{name}}
+end
+return new_array
+end
